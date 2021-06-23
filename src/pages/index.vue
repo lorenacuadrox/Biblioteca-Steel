d1f320aba1f959255cb187e860af7a1b6f721994
Lorena Cuadros <lorena13cuadros@gmail.com>
2021-06-23 09:11:09 -0500

    ultimar detalles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,3 @@
 <template>
     <h1 class="title">Biblioteca</h1>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <router-link to="/author" class="nav-link">Autores</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/book" class="nav-link">Libros</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/editorial" class="nav-link">Editoriales</router-link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
 </template>
